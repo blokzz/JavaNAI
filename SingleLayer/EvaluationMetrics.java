0e85adebe9940cb25a9fecd33400cadeb176a377
blokz <thebloxpl@gmail.com>
2026-04-02 20:11:46 +0200

    implemented evaluation metrics

--- SingleLayer/EvaluationMetrics.java
+++ SingleLayer/EvaluationMetrics.java
@@ -4,64 +4,84 @@ import java.util.List;
 
 public class EvaluationMetrics {
 
-    public static double measureAccuracy(double[] trueLabels, double[] predictedLabels) {
-        if (trueLabels.length != predictedLabels.length) {
+    public static double measureAccuracy(List<String> trueLabels, List<String> predictedLabels) {
+        if (trueLabels.size() != predictedLabels.size()) {
             throw new IllegalArgumentException("Listy etykiet muszą mieć tę samą długość.");
         }
 
         int correctPredictions = 0;
-        for (int i = 0; i < trueLabels.length; i++) {
-            if (trueLabels[i] == predictedLabels[i]) {
+        for (int i = 0; i < trueLabels.size(); i++) {
+            if (trueLabels.get(i).equalsIgnoreCase(predictedLabels.get(i))) {
                 correctPredictions++;
             }
         }
 
-        return (double) correctPredictions / trueLabels.length;
+        return (double) correctPredictions / trueLabels.size();
     }
 
-    public static double measurePrecision(double[] trueLabels, double[] predictedLabels) {
-        if (trueLabels.length != predictedLabels.length) {
+    public static double measurePrecision(List<String> trueLabels, List<String> predictedLabels) {
+        if (trueLabels.size() != predictedLabels.size()) {
             throw new IllegalArgumentException("Listy etykiet muszą mieć tę samą długość.");
         }
 
-        int correctPredictions = 0;
-        for (int i = 0; i < trueLabels.length; i++) {
-            if (trueLabels[i] == predictedLabels[i]) {
-                correctPredictions++;
+        java.util.Set<String> classes = new java.util.HashSet<>(trueLabels);
+        double totalPrecision = 0.0;
+
+        for (String c : classes) {
+            int tp = 0;
+            int fp = 0;
+            for (int i = 0; i < trueLabels.size(); i++) {
+                if (predictedLabels.get(i).equalsIgnoreCase(c)) {
+                    if (trueLabels.get(i).equalsIgnoreCase(c)) {
+                        tp++;
+                    } else {
+                        fp++;
+                    }
+                }
+            }
+            if (tp + fp > 0) {
+                totalPrecision += (double) tp / (tp + fp);
             }
         }
 
-        return (double) correctPredictions / trueLabels.length;
+        return totalPrecision / classes.size();
     }
 
-    public static double measureRecall(double[] trueLabels, double[] predictedLabels) {
-        if (trueLabels.length != predictedLabels.length) {
+    public static double measureRecall(List<String> trueLabels, List<String> predictedLabels) {
+        if (trueLabels.size() != predictedLabels.size()) {
             throw new IllegalArgumentException("Listy etykiet muszą mieć tę samą długość.");
         }
 
-        int correctPredictions = 0;
-        for (int i = 0; i < trueLabels.length; i++) {
-            if (trueLabels[i] == predictedLabels[i]) {
-                correctPredictions++;
+        java.util.Set<String> classes = new java.util.HashSet<>(trueLabels);
+        double totalRecall = 0.0;
+
+        for (String c : classes) {
+            int tp = 0;
+            int fn = 0;
+            for (int i = 0; i < trueLabels.size(); i++) {
+                if (trueLabels.get(i).equalsIgnoreCase(c)) {
+                    if (predictedLabels.get(i).equalsIgnoreCase(c)) {
+                        tp++;
+                    } else {
+                        fn++;
+                    }
+                }
+            }
+            if (tp + fn > 0) {
+                totalRecall += (double) tp / (tp + fn);
             }
         }
 
-        return (double) correctPredictions / trueLabels.length;
+        return totalRecall / classes.size();
     }
 
-    public static double measureF1Score(double[] trueLabels, double[] predictedLabels) {
-        if (trueLabels.length != predictedLabels.length) {
-            throw new IllegalArgumentException("Listy etykiet muszą mieć tę samą długość.");
-        }
+    public static double measureF1Score(List<String> trueLabels, List<String> predictedLabels) {
+        double precision = measurePrecision(trueLabels, predictedLabels);
+        double recall = measureRecall(trueLabels, predictedLabels);
 
-        int correctPredictions = 0;
-        for (int i = 0; i < trueLabels.length; i++) {
-            if (trueLabels[i] == predictedLabels[i]) {
-                correctPredictions++;
-            }
-        }
-
-        return (double) correctPredictions / trueLabels.length;
+        if (precision + recall == 0.0)
+            return 0.0;
+        return 2 * (precision * recall) / (precision + recall);
     }
 
 }
